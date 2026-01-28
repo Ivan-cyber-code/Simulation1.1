cbfa768ac70edc2fed84a9821fb78445d1610c39
Ivan-cyber-code <lapitow2017@gmail.com>
2026-01-28 22:53:54 +0300

    Пункт 17 ревью отработан Переход к пункту 18

--- src/map/Renderer.java
+++ src/map/Renderer.java
@@ -7,17 +7,33 @@ public class Renderer {
     public void showMap(Field field) {
         for (int lines = 0; lines < field.getLines(); lines++) {
             for (int columns = 0; columns < field.getColumns(); columns++) {
-                String cellColor = isCellEven(lines, columns) ? CellColor.BACKGROUND_WHITE : CellColor.BACKGROUND_BLUE;
-                if (field.getEntities().containsKey(new Coordinates(lines, columns))) {
-                    System.out.print(cellColor + getSprite(new Coordinates(lines, columns), field) + CellColor.BACKGROUND_RESET);
+                String cellColor = showCellColor(lines,columns);
+                if (isSprittes(lines, columns, field)) {
+                    showBusyCell(cellColor, lines, columns, field);
                 } else {
-                    System.out.print(cellColor + "   " + CellColor.BACKGROUND_RESET);
+                    showFreeCell(cellColor);
                 }
             }
             System.out.println();
         }
         System.out.println();
 
+    }
+
+    private void showFreeCell(String cellColor) {
+        System.out.print(cellColor + "   " + CellColor.BACKGROUND_RESET);
+    }
+
+    private void showBusyCell(String cellColor, int lines, int columns, Field field) {
+        System.out.print(cellColor + getSprite(new Coordinates(lines, columns), field) + CellColor.BACKGROUND_RESET);
+    }
+
+    private boolean isSprittes(int lines, int columns, Field field) {
+        return field.getEntities().containsKey(new Coordinates(lines, columns));
+    }
+
+    private String showCellColor(int lines, int columns) {
+        return isCellEven(lines, columns) ? CellColor.BACKGROUND_WHITE : CellColor.BACKGROUND_BLUE;
     }
 
     private String getSprite(Coordinates coordinates, Field field) {
